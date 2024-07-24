3816d96e451257a857c1f2cd9154445a75dcc6b9
Juan Luis <juanlu.calderon@gmail.com>
2024-07-24 08:44:05 +0200

    match details api updated

--- routes/api/apiMatches.js
+++ routes/api/apiMatches.js
@@ -119,60 +119,131 @@ router.get(
   async (req, res, next) => {
     const data = req.params;
 
-    console.log(data);
     try {
-      console.log("matchID");
-
-      // Usar parámetros en la consulta para prevenir inyecciones SQL
       const [rows] = await dbConn.promise().execute(
-        `SELECT *
-        FROM matchResults
-        WHERE (Player1ID = 1 AND Player2ID = 2)
-        OR (Player1ID = 2 AND Player2ID = 1);
+        `SELECT 
+        MR.matchID,
+        DATE_FORMAT(MR.matchDate, '%Y-%m-%d %H:%i:%s') AS matchDate,        
+        P1.playerID AS player1ID,
+        P1.playerName AS player1Name,
+        P2.playerID AS player2ID,
+        P2.playerName AS player2Name,
+        S.setID,
+        S.player1Score,
+        S.player2Score,
+        S.winnerID,
+        T.name
+    FROM 
+        matchResults MR
+    JOIN 
+        players P1 ON MR.player1ID = P1.playerID
+    JOIN 
+        players P2 ON MR.player2ID = P2.playerID
+    JOIN 
+        sets S ON MR.matchID = S.matchID
+    JOIN 
+        players PW ON MR.winnerID = PW.playerID
+    LEFT JOIN 
+        Tournaments T ON MR.tournamentID = T.tournamentID
+    WHERE 
+        MR.tournamentID IS NULL
+        AND (
+            (MR.player1ID = ${data.player1Id} AND MR.player2ID = ${data.player2Id}) 
+            OR (MR.player1ID = ${data.player2Id} AND MR.player2ID = ${data.player1Id})
+        );
 `,
         [data.player1Id, data.player2Id]
       );
 
       // Agrupando datos
-      // const groupedData = {
-      //   id: rows[0].matchID,
-      //   date: rows[0].matchDate,
-      //   player1: {
-      //     id: rows[0].player1ID,
-      //     name: rows[0].player1Name,
-      //   },
-      //   player2: {
-      //     id: rows[0].player2ID,
-      //     name: rows[0].player2Name,
-      //   },
-      //   winnerId: 0,
-      //   sets: rows.map((set) => ({
-      //     setId: set.setID,
-      //     player1Score: set.player1Score,
-      //     player2Score: set.player2Score,
-      //     winnerId: set.winnerID,
-      //   })),
-      //   totalSetsPlayer1: 0,
-      //   totalSetsPlayer2: 0,
-      // };
+      function processMatchData(rows) {
+        if (!rows || rows.length === 0) {
+          return [];
+        }
 
-      // const winnerCounter = groupedData.sets.reduce((acc, set) => {
-      //   acc[set.winnerId] = (acc[set.winnerId] || 0) + 1;
-      //   return acc;
-      // }, {});
+        const tournaments = {
+          friendly: [],
+        };
 
-      // const matchWinnerID = Number(
-      //   Object.keys(winnerCounter).reduce((a, b) =>
-      //     winnerCounter[a] > winnerCounter[b] ? a : b
-      //   )
-      // );
+        rows.forEach((row) => {
+          const tournamentName = row.tournamentName || "friendly";
 
-      // groupedData.winnerId = matchWinnerID;
+          if (!tournaments[tournamentName]) {
+            tournaments[tournamentName] = [];
+          }
 
-      // groupedData.totalSetsPlayer1 = winnerCounter[groupedData.player1.id] || 0;
-      // groupedData.totalSetsPlayer2 = winnerCounter[groupedData.player2.id] || 0;
+          let match = tournaments[tournamentName].find(
+            (match) => match.id === row.matchID
+          );
+          if (!match) {
+            match = {
+              id: row.matchID,
+              date: row.matchDate,
+              player1: {
+                id: row.player1ID,
+                name: row.player1Name,
+              },
+              player2: {
+                id: row.player2ID,
+                name: row.player2Name,
+              },
+              winnerId: 0,
+              sets: [],
+              totalSetsPlayer1: 0,
+              totalSetsPlayer2: 0,
+            };
+            tournaments[tournamentName].push(match);
+          }
 
-      res.send(rows);
+          match.sets.push({
+            setId: row.setID,
+            player1Score: row.player1Score,
+            player2Score: row.player2Score,
+            winnerId: row.winnerID,
+          });
+        });
+
+        Object.keys(tournaments).forEach((tournament) => {
+          tournaments[tournament].forEach((match) => {
+            const winnerCounter = match.sets.reduce((acc, set) => {
+              acc[set.winnerId] = (acc[set.winnerId] || 0) + 1;
+              return acc;
+            }, {});
+
+            const matchWinnerID = Number(
+              Object.keys(winnerCounter).reduce((a, b) =>
+                winnerCounter[a] > winnerCounter[b] ? a : b
+              )
+            );
+
+            match.winnerId = matchWinnerID;
+            match.totalSetsPlayer1 = winnerCounter[match.player1.id] || 0;
+            match.totalSetsPlayer2 = winnerCounter[match.player2.id] || 0;
+          });
+        });
+
+        return tournaments;
+      }
+
+      function mapMatches(processedData) {
+        return Object.keys(processedData).map((tournament) => ({
+          tournamentName: tournament,
+          matches: processedData[tournament].map((match) => ({
+            id: match.id,
+            date: match.date,
+            player1: match.player1,
+            player2: match.player2,
+            winnerId: match.winnerId,
+            sets: match.sets,
+            totalSetsPlayer1: match.totalSetsPlayer1,
+            totalSetsPlayer2: match.totalSetsPlayer2,
+          })),
+        }));
+      }
+
+      const processedData = processMatchData(rows);
+      const mappedMatches = mapMatches(processedData);
+      res.send(mappedMatches);
     } catch (err) {
       console.error(err);
       res.status(500).send("Internal Server Error");
